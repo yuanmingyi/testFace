57e0a84ba7a9b65156a919f7c2a195d01c04e451
Mingyi Yuan <millan.yuan@gmail.com>
2012-06-01 06:15:16 +0800

    333

---
+++ testFace/UIAlertView+AlertBox.m
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+AlertBox.m
+//  testFace
+//
+//  Created by Yuan Mingyi on 12-6-1.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIAlertView+AlertBox.h"
+
+@implementation UIAlertView (AlertBox)
+
+@end
