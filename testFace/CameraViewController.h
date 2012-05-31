f6c99ad5f0d72c113b65a7561a9579408e43b0c9
Yuan Mingyi <millan@YuanmatoMac.local>
2012-05-31 23:36:54 +0800

    abc

---
+++ testFace/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  testFace
+//
+//  Created by Mingyi Yuan on 12-5-31.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
---
+++ testFace/CameraViewController.m
@@ -0,0 +1,43 @@
+//
+//  CameraViewController.m
+//  testFace
+//
+//  Created by Mingyi Yuan on 12-5-31.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CameraViewController.h"
+
+@interface CameraViewController ()
+
+@end
+
+@implementation CameraViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
